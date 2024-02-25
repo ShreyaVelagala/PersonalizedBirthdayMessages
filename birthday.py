909db46dc61ae4b4fad01d3b6df1a4d65152cbc5
ShreyaVelagala <38223712+ShreyaVelagala@users.noreply.github.com>
2024-02-25 14:42:24 -0800

    Added some code to get initial openAI API setup

---
+++ birthday.py
@@ -0,0 +1,21 @@
+import os
+from openai import OpenAI
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+client = OpenAI(
+    # This is the default and can be omitted
+    api_key=os.environ.get("OPENAI_API_KEY"),
+)
+
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": "Say this is a test",
+        }
+    ],
+    model="gpt-3.5-turbo",
+)
